88ab7016bbafa22c523039eb3a1446c6adda499b
Michael Chilaka <chilakamichael001@gmail.com>
2019-04-12 16:24:19 +0100

    changes made to js files

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ function changeColor() {
 }
 
 // Completed Random changing of background color
+
+let hexNumbers = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+
+let hexBtn = document.querySelector('.hexBtn');
+let bodyBcg = document.querySelector('body');
+let hex = document.querySelector('.hex');
+
+hexBtn.addEventListener('click', changeColor);
+
+function getHex() {
+    let hexCol = '#';
+    for(let i = 0; i < 6; i++) {
+        let rendom = Math.floor(Math.random()+hexNumbers.length);
+        hexColor += hexNumbers[random];
+        console.log(hexCol);
+    }
+}
+
+// Completed Randomly changing of Hex colors
