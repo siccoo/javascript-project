cc596fd72485e8959da5286412bd4932ef7c1993
Michael Chilaka <chilakamichael001@gmail.com>
2019-04-12 11:30:49 +0100

    js Completed Random changing of background color

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 let colorBtn = document.querySelector('.colorBtn');
 let bodyBg = document.querySelector('body');
+
+let colors = [ 'Yellow', 'Red', 'Green', '#3b5998' ]
+
+
+colorBtn.addEventListener('click', changeColor);
+
+function changeColor() {
+    // bodyBg.style.backgroundColor = colors[1];
+    let random = Math.floor(Math.random()*colors.length);
+    bodyBg.style.backgroundColor = colors[random];
+}
+
+// Completed Random changing of background color
