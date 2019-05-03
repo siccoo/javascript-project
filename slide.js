939b5db3a4ce0838676930af7988c2c3138696b6
Michael Chilaka <chilakamichael001@gmail.com>
2019-05-03 17:01:03 +0100

    slide.css, slide.js, images

--- slide.js
+++ slide.js
@@ -3,3 +3,52 @@ const prevBtn = document.querySelector('.prevBtn');
 const container = document.querySelector('.images');
 
 let counter = 0;
+
+prevBtn.addEventListener('click', prevSlide);
+nextBtn.addEventListener('click', nextSlide);
+
+function prevSlide() {
+
+}
+
+function nextSlide() {
+    if (counter === 4) {
+        counter = -1;
+    }
+
+    container.animate([
+        {
+            opacity: '0.1'
+        },
+        {
+            opacity: '1'
+        }
+    ], {
+        duration: 1000,
+        fill: 'forwards'
+    });
+
+    counter++;
+    container.style.backgroundImage = `url(images/bcg-${counter}.jpg)`
+}
+
+function prevSlide() {
+    if (counter === 0) {
+        counter = 5;
+    }
+
+    container.animate([
+        {
+            opacity: '0.1'
+        },
+        {
+            opacity: '1'
+        }
+    ], {
+        duration: 1000,
+        fill: 'forwards'
+    });
+
+    counter--;
+    container.style.backgroundImage = `url(images/bcg-${counter}.jpg)`
+}
